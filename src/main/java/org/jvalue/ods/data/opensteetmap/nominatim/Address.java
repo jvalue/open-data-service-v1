cdc0812d3c96ee0f796c5e634e8ff06346a178d9
faupat <reischl.patrick@gmail.com>
2014-01-24 03:43:49 +0100

    - reverse geo encoding - missing jautodoc

--- src/main/java/org/jvalue/ods/data/opensteetmap/nominatim/Address.java
+++ src/main/java/org/jvalue/ods/data/opensteetmap/nominatim/Address.java
@@ -1,76 +1,234 @@
+/*  Open Data Service
+    Copyright (C) 2013  Tsysin Konstantin, Reischl Patrick
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+*/
 package org.jvalue.ods.data.opensteetmap.nominatim;
 
-public class Address{
-   	private String city;
-   	private String country;
-   	private String country_code;
-   	private String county;
-   	private String house_number;
-   	private String postcode;
-   	private String road;
-   	private String state;
-   	private String state_district;
-   	private String suburb;
+/**
+ * The Class Address.
+ */
+public class Address {
+	
+	/** The city. */
+	private String city;
+	
+	/** The country. */
+	private String country;
+	
+	/** The country_code. */
+	private String country_code;
+	
+	/** The county. */
+	private String county;
+	
+	/** The house_number. */
+	private String house_number;
+	
+	/** The postcode. */
+	private String postcode;
+	
+	/** The road. */
+	private String road;
+	
+	/** The state. */
+	private String state;
+	
+	/** The state_district. */
+	private String state_district;
+	
+	/** The suburb. */
+	private String suburb;
 
- 	public String getCity(){
+	/**
+	 * Gets the city.
+	 *
+	 * @return the city
+	 */
+	public String getCity() {
 		return this.city;
 	}
-	public void setCity(String city){
+
+	/**
+	 * Sets the city.
+	 *
+	 * @param city the new city
+	 */
+	public void setCity(String city) {
 		this.city = city;
 	}
- 	public String getCountry(){
+
+	/**
+	 * Gets the country.
+	 *
+	 * @return the country
+	 */
+	public String getCountry() {
 		return this.country;
 	}
-	public void setCountry(String country){
+
+	/**
+	 * Sets the country.
+	 *
+	 * @param country the new country
+	 */
+	public void setCountry(String country) {
 		this.country = country;
 	}
- 	public String getCountry_code(){
+
+	/**
+	 * Gets the country_code.
+	 *
+	 * @return the country_code
+	 */
+	public String getCountry_code() {
 		return this.country_code;
 	}
-	public void setCountry_code(String country_code){
+
+	/**
+	 * Sets the country_code.
+	 *
+	 * @param country_code the new country_code
+	 */
+	public void setCountry_code(String country_code) {
 		this.country_code = country_code;
 	}
- 	public String getCounty(){
+
+	/**
+	 * Gets the county.
+	 *
+	 * @return the county
+	 */
+	public String getCounty() {
 		return this.county;
 	}
-	public void setCounty(String county){
+
+	/**
+	 * Sets the county.
+	 *
+	 * @param county the new county
+	 */
+	public void setCounty(String county) {
 		this.county = county;
 	}
- 	public String getHouse_number(){
+
+	/**
+	 * Gets the house_number.
+	 *
+	 * @return the house_number
+	 */
+	public String getHouse_number() {
 		return this.house_number;
 	}
-	public void setHouse_number(String house_number){
+
+	/**
+	 * Sets the house_number.
+	 *
+	 * @param house_number the new house_number
+	 */
+	public void setHouse_number(String house_number) {
 		this.house_number = house_number;
 	}
- 	public String getPostcode(){
+
+	/**
+	 * Gets the postcode.
+	 *
+	 * @return the postcode
+	 */
+	public String getPostcode() {
 		return this.postcode;
 	}
-	public void setPostcode(String postcode){
+
+	/**
+	 * Sets the postcode.
+	 *
+	 * @param postcode the new postcode
+	 */
+	public void setPostcode(String postcode) {
 		this.postcode = postcode;
 	}
- 	public String getRoad(){
+
+	/**
+	 * Gets the road.
+	 *
+	 * @return the road
+	 */
+	public String getRoad() {
 		return this.road;
 	}
-	public void setRoad(String road){
+
+	/**
+	 * Sets the road.
+	 *
+	 * @param road the new road
+	 */
+	public void setRoad(String road) {
 		this.road = road;
 	}
- 	public String getState(){
+
+	/**
+	 * Gets the state.
+	 *
+	 * @return the state
+	 */
+	public String getState() {
 		return this.state;
 	}
-	public void setState(String state){
+
+	/**
+	 * Sets the state.
+	 *
+	 * @param state the new state
+	 */
+	public void setState(String state) {
 		this.state = state;
 	}
- 	public String getState_district(){
+
+	/**
+	 * Gets the state_district.
+	 *
+	 * @return the state_district
+	 */
+	public String getState_district() {
 		return this.state_district;
 	}
-	public void setState_district(String state_district){
+
+	/**
+	 * Sets the state_district.
+	 *
+	 * @param state_district the new state_district
+	 */
+	public void setState_district(String state_district) {
 		this.state_district = state_district;
 	}
- 	public String getSuburb(){
+
+	/**
+	 * Gets the suburb.
+	 *
+	 * @return the suburb
+	 */
+	public String getSuburb() {
 		return this.suburb;
 	}
-	public void setSuburb(String suburb){
+
+	/**
+	 * Sets the suburb.
+	 *
+	 * @param suburb the new suburb
+	 */
+	public void setSuburb(String suburb) {
 		this.suburb = suburb;
 	}
 }
