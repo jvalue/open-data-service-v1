0252e12532b22b17426169e32c778532047206ed
amazing4u <konstantin.tsysin@gmx.de>
2014-03-18 16:32:35 +0100

    added offline grabber test

--- src/integrationtest/integration/org/jvalue/ods/grabber/OsmGrabberTest.java
+++ src/integrationtest/integration/org/jvalue/ods/grabber/OsmGrabberTest.java
@@ -9,7 +9,11 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.jvalue.ods.data.osm.OsmData;
+import org.jvalue.ods.db.DbAccessor;
+import org.jvalue.ods.db.DbFactory;
 import org.jvalue.ods.grabber.OsmGrabber;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 /**
  * The Class OsmGrabberTest.
@@ -39,7 +43,24 @@ public class OsmGrabberTest {
 		OsmData d = grabber.grab(testUrl);
 		assertNotNull(d);
 	}
+	
+	
+	/**
+	 * Test grab.
+	 */
+	@Test
+	public void testGrabOffline() {
+		OsmData d = grabber.grab("nbgcity.osm");
+		assertNotNull(d);
+		DbAccessor<JsonNode> db = DbFactory.createDbAccessor("testOsm");
+		db.connect();
+		db.executeBulk(d.getNodes());
+		db.executeBulk(d.getRelations());
+		db.executeBulk(d.getWays());	
+		db.deleteDatabase();
+	}
 
+	
 	/**
 	 * Test grab invalid source.
 	 */
