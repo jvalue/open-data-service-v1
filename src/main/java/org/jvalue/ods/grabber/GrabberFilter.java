b7d458d9b45added4ab7d87099d4795097822196
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-06-16 15:21:52 +0200

    grabber: added GrabberFilter and GrabberVisitor

---
+++ src/main/java/org/jvalue/ods/grabber/GrabberFilter.java
@@ -0,0 +1,35 @@
+/*  Open Data Service
+    Copyright (C) 2013  Tsysin Konstantin, Reischl Patrick
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+ */
+package org.jvalue.ods.grabber;
+
+import org.jvalue.ods.data.DataSource;
+import org.jvalue.ods.data.generic.GenericEntity;
+import org.jvalue.ods.filter.OdsFilter;
+
+
+public final class GrabberFilter extends OdsFilter<Void, GenericEntity> {
+
+	private final GrabberVisitor visitor = new GrabberVisitor();
+	
+	@Override
+	protected final GenericEntity filterHelper(DataSource source, Void param) {
+		return source.accept(visitor, null).translate(source);
+	}
+
+
+}
