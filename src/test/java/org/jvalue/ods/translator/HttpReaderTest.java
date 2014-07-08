a35d59ea59364143d90569e7bf8723151857183a
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-08 12:05:56 +0200

    test: moved HttpReaderTest to regular tests from integrationTest

--- src/test/java/org/jvalue/ods/translator/HttpReaderTest.java
+++ src/test/java/org/jvalue/ods/translator/HttpReaderTest.java
@@ -15,7 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
  */
-package integration.org.jvalue.ods.translator;
+package org.jvalue.ods.translator;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -23,7 +23,6 @@ import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.jvalue.ods.translator.HttpReader;
 
 /**
  * The Class HttpReaderTest.
