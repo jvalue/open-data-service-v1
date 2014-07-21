de0d735500e3949cec88c97ad52030082003bfbe
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-21 21:20:45 +0200

    integrationtest: fixed ClientDatastoreTest

--- src/integrationtest/integration/org/jvalue/ods/notifications/db/ClientDatastoreTest.java
+++ src/integrationtest/integration/org/jvalue/ods/notifications/db/ClientDatastoreTest.java
@@ -9,7 +9,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.jvalue.ods.notifications.clients.Client;
-import org.jvalue.ods.notifications.clients.GcmClient;
+import org.jvalue.ods.notifications.clients.ClientFactory;
 import org.jvalue.ods.notifications.db.ClientDatastore;
 import org.jvalue.ods.notifications.db.ClientDatastoreFactory;
 
@@ -21,7 +21,7 @@ public final class ClientDatastoreTest {
 	public static final void clearDatabase() {
 		ClientDatastore store = ClientDatastoreFactory.getClientDatastore();
 		for (Client client : store.getAll()) {
-			store.remove(client);
+			store.remove(client.getClientId());
 		}
 	}
 
@@ -31,23 +31,23 @@ public final class ClientDatastoreTest {
 		ClientDatastore store = ClientDatastoreFactory.getClientDatastore();
 
 		List<Client> clients = new ArrayList<Client>();
-		clients.add(new GcmClient("foo", "pegelonline"));
-		clients.add(new GcmClient("bar", "pegelonline"));
-		clients.add(new GcmClient("bar", "pegeloffline"));
-		clients.add(new GcmClient("foobar", "nopegel"));
+		clients.add(ClientFactory.newGcmClient("pegelonline", "foo"));
+		clients.add(ClientFactory.newGcmClient("pegelonline", "bar"));
+		clients.add(ClientFactory.newGcmClient("pegeloffline", "bar"));
+		clients.add(ClientFactory.newGcmClient("nopegel", "foobar"));
 
 		for (Client client : clients) {
-			assertTrue(!store.contains(client));
+			assertTrue(!store.contains(client.getClientId()));
 			assertTrue(!store.getAll().contains(client));
 
 			store.add(client);
 
-			assertTrue(store.contains(client));
+			assertTrue(store.contains(client.getClientId()));
 			assertTrue(store.getAll().contains(client));
 
-			store.remove(client);
+			store.remove(client.getClientId());
 
-			assertTrue(!store.contains(client));
+			assertTrue(!store.contains(client.getClientId()));
 			assertTrue(!store.getAll().contains(client));
 		}
 	}
