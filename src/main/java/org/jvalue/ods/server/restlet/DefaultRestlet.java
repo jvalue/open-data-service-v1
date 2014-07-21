7a4570aa994f1e9b9e5e4b0be4e8e96e7f55dae5
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-21 12:01:11 +0200

    restlet: more cleaning in DefaultRestlet

--- src/main/java/org/jvalue/ods/server/restlet/DefaultRestlet.java
+++ src/main/java/org/jvalue/ods/server/restlet/DefaultRestlet.java
@@ -17,50 +17,25 @@
  */
 package org.jvalue.ods.server.restlet;
 
-import org.jvalue.ods.db.DbAccessor;
-import org.jvalue.ods.db.DbFactory;
 import org.jvalue.ods.server.utils.RestletResult;
 import org.restlet.Request;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 
 public class DefaultRestlet extends BaseRestlet {
 
 	@Override
 	protected RestletResult doGet(Request request) {
-		String url = request.getResourceRef().toString();
+		return handle();
+	}
 
-		String[] tree = url.split("/");
+	
+	@Override
+	protected RestletResult doPost(Request request) {
+		return handle();
+	}
 
-		if (url.contains("/ods/$")) {
 
-			DbAccessor<JsonNode> dbAccessor = DbFactory.createDbAccessor("ods");
-			dbAccessor.connect();
-
-			// determine position where intern path begins
-			int pos = 0;
-			for (int i = 0; i < tree.length; i++) {
-				if (tree[i].startsWith("$")) {
-					pos = i;
-					break;
-				}
-			}
-
-			JsonNode node = null;
-			String objectId = tree[pos].substring(1);
-			try {
-				node = dbAccessor.getDocument(JsonNode.class, objectId);
-				for (int k = pos + 1; k < tree.length; k++) {
-					node = node.get(tree[k]);
-				}
-				return RestletResult.newSuccessResult(node);
-
-			} catch (RuntimeException e) {
-				return onBadRequest("no data for given id '" + objectId + "'");
-			}
-		}
-
+	private RestletResult handle() {
 		return onBadRequest("Invalid query, see /api for available queries");
 	}
 
