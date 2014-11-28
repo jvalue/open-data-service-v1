69ebfc97185878685cc13a7f34d3c717dff40e95
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-11-28 14:13:11 +0100

    data: added JSON serializing to JsonPointer
    Why doesn't jackson support this natively??

---
+++ src/main/java/org/jvalue/ods/utils/JsonPointerDeserializer.java
@@ -0,0 +1,18 @@
+package org.jvalue.ods.utils;
+
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+
+public final class JsonPointerDeserializer extends JsonDeserializer<JsonPointer> {
+
+	@Override
+	public JsonPointer deserialize(JsonParser jsonParser, DeserializationContext context) throws IOException {
+		return JsonPointer.compile(jsonParser.getText());
+	}
+
+}
