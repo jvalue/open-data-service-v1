8e580c9ddd6508969a9a798760a00320a27eec9e
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-11-29 18:40:01 +0100

    rest: data and filter api use PUT to create new instances

--- src/main/java/org/jvalue/ods/rest/DataSourceApi.java
+++ src/main/java/org/jvalue/ods/rest/DataSourceApi.java
@@ -1,17 +1,29 @@
 package org.jvalue.ods.rest;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.inject.Inject;
 
+import org.ektorp.DocumentNotFoundException;
 import org.jvalue.ods.data.DataSource;
+import org.jvalue.ods.data.DataSourceMetaData;
 import org.jvalue.ods.db.DataSourceRepository;
+import org.jvalue.ods.utils.Assert;
+import org.jvalue.ods.utils.JsonPointerDeserializer;
+import org.jvalue.ods.utils.JsonPointerSerializer;
 
+import java.net.URL;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -43,9 +55,24 @@ public final class DataSourceApi extends AbstractApi {
 	}
 
 
-	@POST
-	public void addSource(DataSource dataSource) {
-		sourceRepository.add(dataSource);
+	@PUT
+	@Path("/{sourceId}")
+	public DataSource addSource(@PathParam("sourceId") String sourceId, DataSourceDescription sourceDescription) {
+		try {
+			sourceRepository.findBySourceId(sourceId);
+			throw RestUtils.createJsonFormattedException("source with id " + sourceId + " already exists", 409);
+		} catch (DocumentNotFoundException dnfe) {
+			// source does not exist --> continue;
+		}
+
+		DataSource source = new DataSource(
+				sourceId,
+				sourceDescription.url,
+				sourceDescription.domainIdKey,
+				sourceDescription.schema,
+				sourceDescription.metaData);
+		sourceRepository.add(source);
+		return source;
 	}
 
 
@@ -53,6 +80,31 @@ public final class DataSourceApi extends AbstractApi {
 	@Path("/{sourceId}")
 	public void deleteSource(@PathParam("sourceId") String sourceId) {
 		sourceRepository.remove(sourceRepository.findBySourceId(sourceId));
+	}
+
+
+	private static final class DataSourceDescription {
+		private final URL url;
+		private final DataSourceMetaData metaData;
+		@JsonSerialize(using = JsonPointerSerializer.class)
+		@JsonDeserialize(using = JsonPointerDeserializer.class)
+		private final JsonPointer domainIdKey;
+		private final JsonNode schema;
+
+		@JsonCreator
+		public DataSourceDescription(
+				@JsonProperty("url") URL url,
+				@JsonProperty("domainIdKey") JsonPointer domainIdKey,
+				@JsonProperty("schema") JsonNode schema,
+				@JsonProperty("metaData") DataSourceMetaData metaData) {
+
+			Assert.assertNotNull(url, domainIdKey, schema, metaData);
+			this.url = url;
+			this.domainIdKey = domainIdKey;
+			this.schema = schema;
+			this.metaData = metaData;
+		}
+
 	}
 
 }
--- src/main/java/org/jvalue/ods/rest/FilterChainApi.java
+++ src/main/java/org/jvalue/ods/rest/FilterChainApi.java
@@ -4,6 +4,7 @@ package org.jvalue.ods.rest;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.inject.Inject;
 
+import org.ektorp.DocumentNotFoundException;
 import org.jvalue.ods.data.DataSource;
 import org.jvalue.ods.db.DataSourceRepository;
 import org.jvalue.ods.db.FilterChainReferenceRepository;
@@ -17,7 +18,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -61,14 +62,23 @@ public final class FilterChainApi extends AbstractApi {
 	}
 
 
-	@POST
+	@PUT
+	@Path("/{filterChainId}")
 	public FilterChainReference addFilterChain(
 			@PathParam("sourceId") String sourceId,
+			@PathParam("filterChainId") String filterChainId,
 			FilterChainReferenceDescription description) {
+
+		try {
+			referenceRepository.findByFilterChainId(filterChainId);
+			throw RestUtils.createJsonFormattedException("filter chain with id " + filterChainId + " already exists", 409);
+		} catch(DocumentNotFoundException dnfe) {
+			// chain not present --> continue;
+		}
 
 		DataSource source = sourceRepository.findBySourceId(sourceId);
 		List<FilterReference> filterReferences = new LinkedList<>();
-		for (String filterName : description.getFilterNames()) {
+		for (String filterName : description.filterNames) {
 			FilterReference reference = referenceManager.getFilterReferenceByName(filterName);
 			if (reference == null) {
 				throw RestUtils.createJsonFormattedException("unknown filter '" + filterName + "'", 400);
@@ -78,7 +88,7 @@ public final class FilterChainApi extends AbstractApi {
 
 		try {
 			FilterChainReference chainReference = referenceManager.createFilterChainReference(
-					description.getFilterChainId(),
+					filterChainId,
 					filterReferences,
 					new FilterChainMetaData(-1),
 					source.getSourceId());
@@ -93,26 +103,13 @@ public final class FilterChainApi extends AbstractApi {
 
 	private static final class FilterChainReferenceDescription {
 
-		private final String filterChainId;
 		private final List<String> filterNames;
 
 		public FilterChainReferenceDescription(
-				@JsonProperty("filterChainId") String filterChainId,
 				@JsonProperty("filterNames") List<String> filterNames) {
 
-			Assert.assertNotNull(filterChainId, filterNames);
-			this.filterChainId = filterChainId;
+			Assert.assertNotNull(filterNames);
 			this.filterNames = filterNames;
-		}
-
-
-		public String getFilterChainId() {
-			return filterChainId;
-		}
-
-
-		public List<String> getFilterNames() {
-			return filterNames;
 		}
 
 	}
