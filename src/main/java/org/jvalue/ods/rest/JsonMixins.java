fd1ce6556bfbc156c8e8a9948a96eae64f3bc16f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-11-30 14:50:02 +0100

    rest: renamed ids for DataSource and FilterChainReference to "id"

--- src/main/java/org/jvalue/ods/rest/JsonMixins.java
+++ src/main/java/org/jvalue/ods/rest/JsonMixins.java
@@ -1,8 +1,11 @@
 package org.jvalue.ods.rest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.ektorp.support.CouchDbDocument;
+import org.jvalue.ods.data.DataSource;
+import org.jvalue.ods.filter.reference.FilterChainReference;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,11 +16,23 @@ public class JsonMixins {
 	@JsonIgnoreProperties({"_id", "_rev"})
 	private static interface CouchDbMixin { }
 
+	@JsonIgnoreProperties({"_id", "_rev"})
+	private static interface DataSourceMixin {
+		@JsonProperty("id") String getSourceId();
+	}
+
+	@JsonIgnoreProperties({"_id", "_rev"})
+	private static interface FilterChainReferenceMixin {
+		@JsonProperty("id") String getFilterChainId();
+	}
+
 
 	private final Map<Class<?>, Class<?>> mixins = new HashMap<>();
 
 	public JsonMixins() {
 		mixins.put(CouchDbDocument.class, CouchDbMixin.class);
+		mixins.put(DataSource.class, DataSourceMixin.class);
+		mixins.put(FilterChainReference.class, FilterChainReferenceMixin.class);
 	}
 
 
