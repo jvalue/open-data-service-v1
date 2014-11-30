7924c56403175455e88e4dbd258ce14e924b2e67
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-11-30 13:20:27 +0100

    rest: removed custom mixin for FilterChains in JsonMixins

--- src/main/java/org/jvalue/ods/rest/JsonMixins.java
+++ src/main/java/org/jvalue/ods/rest/JsonMixins.java
@@ -3,7 +3,6 @@ package org.jvalue.ods.rest;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.ektorp.support.CouchDbDocument;
-import org.jvalue.ods.filter.reference.FilterChainReference;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,15 +13,11 @@ public class JsonMixins {
 	@JsonIgnoreProperties({"_id", "_rev"})
 	private static interface CouchDbMixin { }
 
-	@JsonIgnoreProperties({"_id", "_rev", "dataSourceId"})
-	private static interface FilterChainReferenceMixin { }
-
 
 	private final Map<Class<?>, Class<?>> mixins = new HashMap<>();
 
 	public JsonMixins() {
 		mixins.put(CouchDbDocument.class, CouchDbMixin.class);
-		mixins.put(FilterChainReference.class, FilterChainReferenceMixin.class);
 	}
 
 
