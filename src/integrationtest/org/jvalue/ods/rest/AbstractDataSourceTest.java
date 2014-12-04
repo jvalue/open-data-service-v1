9141094e7ccd2c3561858907875de911a96209ae
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-12-04 16:34:47 +0100

    test: added AbstractDataSourceTest for handling source lifecycle

---
+++ src/integrationtest/org/jvalue/ods/rest/AbstractDataSourceTest.java
@@ -0,0 +1,49 @@
+package org.jvalue.ods.rest;
+
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.jvalue.ods.data.DataSourceMetaData;
+import org.jvalue.ods.rest.retrofit.ClientFactory;
+import org.jvalue.ods.rest.retrofit.DataSource;
+import org.jvalue.ods.rest.retrofit.DataSourceClient;
+
+import java.net.URL;
+import java.util.UUID;
+
+
+public abstract class AbstractDataSourceTest {
+
+	protected static final ClientFactory clientFactory = new ClientFactory();
+	protected static final DataSourceClient sourceClient = clientFactory.getDataSourceClient();
+
+	protected static final DataSource source = new DataSource();
+	protected static String sourceId = "TEST_" + UUID.randomUUID().toString();
+
+	static {
+		try {
+			source.url = new URL("http://localhost:8080/");
+			source.metaData = new DataSourceMetaData("", "", "", "", "", "", "");
+			source.domainIdKey = "/someId";
+			source.schema = new ObjectNode(JsonNodeFactory.instance);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	@BeforeClass
+	public static void addDataSource() {
+		sourceClient.add(sourceId, source);
+	}
+
+
+	@AfterClass
+	public static void removeDataSource() {
+		sourceClient.remove(sourceId);
+	}
+
+}
--- src/integrationtest/org/jvalue/ods/rest/DataSourceApiTest.java
+++ src/integrationtest/org/jvalue/ods/rest/DataSourceApiTest.java
@@ -1,68 +1,52 @@
 package org.jvalue.ods.rest;
 
 
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.jvalue.ods.data.DataSourceMetaData;
-import org.jvalue.ods.rest.retrofit.ClientFactory;
 import org.jvalue.ods.rest.retrofit.DataSource;
-import org.jvalue.ods.rest.retrofit.DataSourceClient;
 
-import java.net.URL;
 import java.util.List;
 
 import retrofit.RetrofitError;
 
-public final class DataSourceApiTest {
+public final class DataSourceApiTest extends AbstractDataSourceTest {
 
 	@Test
 	public void testCrud() throws Exception {
-		DataSourceClient client = new ClientFactory().getDataSourceClient();
-
-		String sourceId = DataSourceApiTest.class.getSimpleName();
-
-		DataSource sentSource = new DataSource();
-		sentSource.url = new URL("http://localhost:8080/");
-		sentSource.metaData = new DataSourceMetaData("", "", "", "", "", "", "");
-		sentSource.domainIdKey = "/someId";
-		sentSource.schema = new ObjectNode(JsonNodeFactory.instance);
-
 		{
-			// add and get source
-			client.add(sourceId, sentSource);
-			DataSource receivedSource = client.get(sourceId);
-			assertEquals(sentSource, receivedSource);
+			// get source
+			DataSource receivedSource = sourceClient.get(sourceId);
+			assertEquals(source, receivedSource);
 			Assert.assertEquals(sourceId, receivedSource.id);
 		}
 
 		{
 			// get all sources
-			List<DataSource> sources = client.getAll();
-			DataSource source = null;
+			List<DataSource> sources = sourceClient.getAll();
+			DataSource receivedSource = null;
 			for (DataSource s : sources) {
 				if (sourceId.equals(s.id)) {
-					source = s;
+					receivedSource = s;
 					break;
 				}
 			}
-			Assert.assertNotNull("failed to find source in getAll", source);
-			assertEquals(sentSource, source);
+			Assert.assertNotNull("failed to find source in getAll", receivedSource);
+			assertEquals(source, receivedSource);
 		}
 
 		{
 			// delete source
-			client.remove(sourceId);
+			removeDataSource();
 			try {
-				client.get(sourceId);
+				sourceClient.get(sourceId);
 				Assert.fail("source not removed but should be");
 			} catch (RetrofitError re) {
 				Assert.assertEquals(404, re.getResponse().getStatus());
 			}
+			addDataSource(); // otherwise afterClass method will be in a bad mood
 		}
 	}
+
 
 	private void assertEquals(DataSource source1, DataSource source2) {
 		Assert.assertEquals(source1.url, source2.url);
