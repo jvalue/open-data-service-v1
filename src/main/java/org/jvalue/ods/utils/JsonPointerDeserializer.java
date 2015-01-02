c8a1616b443f326612545b09e4c09cad5fe92f10
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-01-02 18:16:04 +0100

    utils: no more 500 on invalid JsonPointers

--- src/main/java/org/jvalue/ods/utils/JsonPointerDeserializer.java
+++ src/main/java/org/jvalue/ods/utils/JsonPointerDeserializer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 import java.io.IOException;
 
@@ -12,7 +13,11 @@ public final class JsonPointerDeserializer extends JsonDeserializer<JsonPointer>
 
 	@Override
 	public JsonPointer deserialize(JsonParser jsonParser, DeserializationContext context) throws IOException {
-		return JsonPointer.compile(jsonParser.getText());
+		try {
+			return JsonPointer.compile(jsonParser.getText());
+		} catch (Exception e) {
+			throw new JsonMappingException(e.getMessage());
+		}
 	}
 
 }
