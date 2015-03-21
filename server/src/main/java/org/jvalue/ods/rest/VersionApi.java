dc14120677dd3225dd6145a2046991f9c7977314
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-03-22 00:03:17 +0100

    Move VersionInfo to common module

--- server/src/main/java/org/jvalue/ods/rest/VersionApi.java
+++ server/src/main/java/org/jvalue/ods/rest/VersionApi.java
@@ -2,7 +2,7 @@ package org.jvalue.ods.rest;
 
 
 import org.jvalue.ods.GitConstants;
-import org.jvalue.ods.api.OdsVersion;
+import org.jvalue.common.utils.VersionInfo;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,14 +13,13 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public final class VersionApi {
 
-	private static final OdsVersion version = new OdsVersion(
-			"0.1.0-SNAPSHOT",
-			1,
+	private static final VersionInfo version = new VersionInfo(
+			GitConstants.VERSION,
 			"https://github.com/jvalue/open-data-service/commit/" +  GitConstants.COMMIT_HASH);
 
 
 	@GET
-	public OdsVersion getVersion() {
+	public VersionInfo getVersion() {
 		return version;
 	}
 
