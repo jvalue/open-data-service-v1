a3317e408d4c7f7535252ec93a9b773b6a114ced
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-06-19 22:05:52 +0200

    processor: reformat date ein Brandenburg merger

--- server/src/main/java/org/jvalue/ods/processor/filter/domain/PegelBrandenburgMerger.java
+++ server/src/main/java/org/jvalue/ods/processor/filter/domain/PegelBrandenburgMerger.java
@@ -4,9 +4,15 @@ import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.assistedinject.Assisted;
 
+import org.jvalue.commons.utils.Assert;
+import org.jvalue.commons.utils.Log;
 import org.jvalue.ods.api.sources.DataSource;
 import org.jvalue.ods.processor.filter.AbstractDataModifierFilter;
 import org.jvalue.ods.processor.filter.FilterException;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.inject.Inject;
 
@@ -16,6 +22,11 @@ import javax.inject.Inject;
  * to be merge with other water level * data sources.
  */
 public class PegelBrandenburgMerger extends AbstractDataModifierFilter {
+
+	private static final DateFormat
+			sourceDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm"),
+			targetDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
+
 
 	@Inject
 	PegelBrandenburgMerger(@Assisted DataSource source, MetricRegistry registry) {
@@ -41,6 +52,20 @@ public class PegelBrandenburgMerger extends AbstractDataModifierFilter {
 
 		newMeasurements.putArray("characteristicValues");
 
+		// reformat timestamp
+		String timestamp = newMeasurements.path("timestamp").asText();
+		if (timestamp.equals("-")) {
+			newMeasurements.remove("timestamp");
+		}
+		else {
+			try {
+				newMeasurements.put("timestamp", targetDateFormat.format(sourceDateFormat.parse(timestamp)));
+			} catch (ParseException p) {
+				Log.warn("failed to parse timestamp " + timestamp);
+				newMeasurements.remove("timestamp");
+			}
+		}
+
 		// remove flowrate
 		data.remove("flowrate");
 
@@ -56,12 +81,23 @@ public class PegelBrandenburgMerger extends AbstractDataModifierFilter {
 		// rename rivershed to water
 		rename(data, "rivershed", "water");
 
+
 		return data;
 	}
 
 	@Override
 	protected void doOnComplete() throws FilterException {
 		// nothing to do
+	}
+
+
+	private long parseStringTimestamp(String stringTimestamp) {
+		Assert.assertNotNull(stringTimestamp);
+		try {
+			return targetDateFormat.parse(stringTimestamp).getTime();
+		} catch (ParseException pe) {
+			throw new IllegalArgumentException(pe);
+		}
 	}
 
 }
