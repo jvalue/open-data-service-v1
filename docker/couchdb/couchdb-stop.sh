e8bd616d0298ff843f5b2fa788a897c5930d5748
Andreas Bauer <andi.bauer@fau.de>
2018-03-15 20:58:13 +0100

    Remove couchdb start/stop scripts, because docker-compose handle this now

--- docker/couchdb/couchdb-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-name="ods-couchdb"
-state=$(docker inspect --format "{{.State.Running}}" $name 2>/dev/null)
-if [[ "$state" == "true" ]]; then
-	echo "Container '$name' is running. Stop and remove container..."
-	sudo docker stop $name 1>/dev/null
-	sudo docker rm $name 1>/dev/null
-fi
