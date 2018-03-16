c2e58103b28c791ad52c662907b57f0812f738a3
Andreas Bauer <andi.bauer@fau.de>
2018-03-16 23:40:47 +0100

    Refactor Docker image     * use now docker-compose-wait to check if service is available

--- docker/ods/assets/start.sh
+++ docker/ods/assets/start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
+# Set config if not set
 /bin/sh set-config.sh
 
-if [ -z "$COUCHDB_HOST" ]; then
-    echo "ERROR: ENV COUCHDB_HOST not set!"
-    exit 1
-fi
-
-while ! nc -z $COUCHDB_HOST 5984;
-    do
-        echo "waiting for CouchDB [$COUCHDB_HOST:5984]  ";
-        sleep 1;
-    done;
-        echo CouchDB is ready!;
-        /usr/bin/java -jar /ods.jar server ods-configuration.yml
+# Start ODS service
+/usr/bin/java -jar /ods.jar server ods-configuration.yml
