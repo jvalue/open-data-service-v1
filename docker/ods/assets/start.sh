63e66f8cca44ac87e40d8236bdcdd86348939971
Andreas Bauer <andi.bauer@fau.de>
2018-03-16 17:47:58 +0100

    Refactor start script for docker image to consider COUCHDB_HOST env

--- docker/ods/assets/start.sh
+++ docker/ods/assets/start.sh
@@ -2,9 +2,14 @@
 
 /bin/sh set-config.sh
 
- while ! nc -z couchdb 5984;
-     do
-        echo "waiting for CouchDB...";
+if [ -z "$COUCHDB_HOST" ]; then
+    echo "ERROR: ENV COUCHDB_HOST not set!"
+    exit 1
+fi
+
+while ! nc -z $COUCHDB_HOST 5984;
+    do
+        echo "waiting for CouchDB [$COUCHDB_HOST:5984]  ";
         sleep 1;
     done;
         echo CouchDB is ready!;
