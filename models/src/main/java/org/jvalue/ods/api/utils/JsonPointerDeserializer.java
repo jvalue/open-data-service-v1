63c9eb181a7575c4f2323c274026fd1bbbc0918d
Andreas Bauer <andi.bauer@fau.de>
2018-03-22 10:16:04 +0100

    Remove deprecated method calls from Json deserializer

--- models/src/main/java/org/jvalue/ods/api/utils/JsonPointerDeserializer.java
+++ models/src/main/java/org/jvalue/ods/api/utils/JsonPointerDeserializer.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 import java.io.IOException;
 
@@ -13,11 +12,7 @@ public final class JsonPointerDeserializer extends JsonDeserializer<JsonPointer>
 
 	@Override
 	public JsonPointer deserialize(JsonParser jsonParser, DeserializationContext context) throws IOException {
-		try {
-			return JsonPointer.compile(jsonParser.getText());
-		} catch (Exception e) {
-			throw new JsonMappingException(e.getMessage());
-		}
+		return JsonPointer.compile(jsonParser.getText());
 	}
 
 }
