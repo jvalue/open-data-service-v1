163b8dacdeebb4bc25328d94416316a81fc026e7
mathias <mathias.zinnen@gmail.com>
2018-06-24 21:29:53 +0200

    refactor jsonApiResourceObjects and implement JsonApiResponse class

---
+++ server/src/main/java/org/jvalue/ods/rest/v2/JsonApi/JsonApiResource.java
@@ -0,0 +1,52 @@
+package org.jvalue.ods.rest.v2.JsonApi;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.Optional;
+
+import static org.jvalue.ods.utils.JsonUtils.getIdFromObject;
+import static org.jvalue.ods.utils.JsonUtils.getPropertyValueNode;
+import static org.jvalue.ods.utils.StringUtils.getUriRootElement;
+
+public class JsonApiResource<T> {
+    private final String selfReference;
+    private final String id;
+    private final String type;
+    private final T entity;
+    private final Optional<JsonNode> meta;
+
+    public JsonApiResource(T entity, String uri, String id) {
+        this.entity = entity;
+        this.selfReference = uri;
+        this.type = getUriRootElement(uri);
+        System.out.println(entity.getClass().getCanonicalName());
+        this.id = id;
+        this.meta = Optional.ofNullable(getPropertyValueNode(entity, "metaData"));
+    }
+
+    public JsonApiResource(T entity, String uri) {
+        this(entity, uri, getIdFromObject(entity));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("attributes")
+    @JsonIgnoreProperties(value = {"id", "metaData"})
+    public T getEntity() {
+        return entity;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_ABSENT)
+    public Optional<JsonNode> getMeta() {
+        return meta;
+    }
+}
