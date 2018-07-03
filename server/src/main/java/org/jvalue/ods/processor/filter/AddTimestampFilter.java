0801493a3bd3e90a009fe6104cc2d529d83ddd15
Andreas Bauer <dev@andreasbauer.org>
2018-07-03 17:08:33 +0200

    Remove unused imports

--- server/src/main/java/org/jvalue/ods/processor/filter/AddTimestampFilter.java
+++ server/src/main/java/org/jvalue/ods/processor/filter/AddTimestampFilter.java
@@ -1,7 +1,6 @@
 package org.jvalue.ods.processor.filter;
 
 import com.codahale.metrics.MetricRegistry;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.assistedinject.Assisted;
 import org.jvalue.ods.api.sources.DataSource;
