99484fdf2f2dd37a86a27b64921ef8626ab6827c
Andreas Bauer <dev@andreasbauer.org>
2018-07-03 16:29:27 +0200

    Clean up Publisher

--- server/src/main/java/org/jvalue/ods/pubsub/Publisher.java
+++ server/src/main/java/org/jvalue/ods/pubsub/Publisher.java
@@ -8,62 +8,64 @@ import org.jvalue.commons.utils.Log;
 
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
 public class Publisher {
 
-    private Connection connection;
-    private Channel channel;
-    @NotNull private String exchange;
-    @NotNull private String host;
+	private Connection connection;
+	private Channel channel;
+	@NotNull private String exchange;
+	@NotNull private String host;
 
-    public Publisher() {
-    }
-
-
-    public boolean connect(String host, String exchange) {
-        this.host = host;
-        this.exchange = exchange;
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(host);
-
-        try {
-            Log.info("Connect to publish/subscribe server: " + toString());
-            connection = factory.newConnection();
-            channel = connection.createChannel();
-            channel.exchangeDeclare(exchange, BuiltinExchangeType.FANOUT);
-        } catch (IOException | TimeoutException e) {
-            Log.error("Unable to connect to publish/subscribe server: " + toString());
-            return false;
-        }
-
-        return true;
-    }
+	public Publisher() {
+	}
 
 
-    public boolean publish(String message) {
-        try {
-            channel.basicPublish(exchange, "", null, message.getBytes());
-            Log.debug("[x] Sent '" + message + "' to " + toString());
-            return true;
-        } catch (NullPointerException | IOException e) {
-            Log.debug("[ ] Sent failed '" + message + "' to " + toString());
-            return false;
-        }
-    }
+	public boolean connect(String host, String exchange) {
+		this.host = host;
+		this.exchange = exchange;
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(host);
+
+		try {
+			Log.info("Connect to publish/subscribe server: " + toString());
+			connection = factory.newConnection();
+			channel = connection.createChannel();
+			channel.exchangeDeclare(exchange, BuiltinExchangeType.FANOUT);
+		} catch (IOException | TimeoutException e) {
+			Log.error("Unable to connect to publish/subscribe server: " + toString());
+			return false;
+		}
+
+		return true;
+	}
 
 
-    public void close() {
-        Log.info("Close connection to publish/subscribe server: " + toString());
-        try {
-            if (channel != null && channel.isOpen()) channel.close();
-            if (connection != null && connection.isOpen()) connection.close();
-        } catch (IOException | TimeoutException e) {
-        }
-    }
+	public boolean publish(String message) {
+		try {
+			channel.basicPublish(exchange, "", null, message.getBytes(StandardCharsets.UTF_8));
+			Log.debug("[x] Sent '" + message + "' to " + toString());
+			return true;
+		} catch (NullPointerException | IOException e) {
+			Log.debug("[ ] Sent failed '" + message + "' to " + toString());
+			return false;
+		}
+	}
 
 
-    public String toString() {
-        return "{host: '"+host+"', exchange_name: '"+exchange+"'}";
-    }
+	public void close() {
+		Log.info("Close connection to publish/subscribe server: " + toString());
+		try {
+			if (channel != null && channel.isOpen()) channel.close();
+			if (connection != null && connection.isOpen()) connection.close();
+		} catch (IOException | TimeoutException e) {
+		}
+	}
+
+
+	public String toString() {
+		return "{host: '" + host + "', exchange_name: '" + exchange + "'}";
+	}
+
 }
