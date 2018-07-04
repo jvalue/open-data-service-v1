c0b739ef1b382216374c4f54d2724f6a650f1d11
Julian Schneider <julian.js.schneider@fau.de>
2018-07-04 12:31:33 +0200

    removed URI

--- server/src/main/java/org/jvalue/ods/transformation/NashornExecutionEngine.java
+++ server/src/main/java/org/jvalue/ods/transformation/NashornExecutionEngine.java
@@ -6,13 +6,16 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import delight.nashornsandbox.NashornSandbox;
 import delight.nashornsandbox.NashornSandboxes;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.jvalue.commons.utils.Log;
 
 import javax.script.ScriptException;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,14 +28,9 @@ public class NashornExecutionEngine extends AbstractExecutionEngine {
 
 	public NashornExecutionEngine() {
 		objectMapper = new ObjectMapper();
-		URL resource = NashornExecutionEngine.class.getClassLoader().getResource("js/JsonUtil.js");
-		File wrapperScriptJs;
+		InputStream resource = NashornExecutionEngine.class.getClassLoader().getResourceAsStream("js/JsonUtil.js");
 		try {
-			wrapperScriptJs = new File(resource.toURI());
-			wrapperScript = FileUtils.readFileToString(wrapperScriptJs);
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-			Log.error(e.getMessage());
+			wrapperScript = IOUtils.toString(resource);
 		} catch (IOException e) {
 			e.printStackTrace();
 			Log.error(e.getMessage());
