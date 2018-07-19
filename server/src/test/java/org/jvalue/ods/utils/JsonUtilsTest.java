5f6e8a50167cae31055d54cf919182a854dda382
mathias <mathias.zinnen@gmail.com>
2018-07-19 20:34:08 +0200

    add JsonUtils to prepare attribute restriction for responses

---
+++ server/src/test/java/org/jvalue/ods/utils/JsonUtilsTest.java
@@ -0,0 +1,46 @@
+package org.jvalue.ods.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Assert;
+import org.junit.Test;
+import org.jvalue.ods.api.jsonapi.JsonApiIdentifiable;
+
+public class JsonUtilsTest {
+
+    private final Dummy dummyObj = new Dummy();
+
+    @Test
+    public void testCreateRestrictedEntity() {
+        JsonNode result = JsonUtils.createRestrictedEntity(dummyObj, "nestedDummy");
+
+        Assert.assertEquals(1, result.size());
+        Assert.assertTrue(result.has("nestedDummy"));
+        Assert.assertEquals(2, result.get("nestedDummy").size());
+    }
+
+
+    private class Dummy implements JsonApiIdentifiable {
+        private final String id = "ID";
+        private final int anotherAttribute = 1;
+        private final NestedDummy nestedDummy = new NestedDummy();
+
+        public int getAnotherAttribute() {
+            return anotherAttribute;
+        }
+
+        public NestedDummy getNestedDummy() {
+            return nestedDummy;
+        }
+
+        @Override
+        public String getId() {
+            return null;
+        }
+    }
+
+
+    private class NestedDummy {
+        public final String stringAttr = "String";
+        public final int intAttr = 0;
+    }
+}
