089ca46d94fc4b2019f2c37cf3ecb35fe24438d1
Julian Schneider <julian.js.schneider@fau.de>
2018-07-28 20:14:27 +0200

    added integration tests for notificationclientrepo and processorchainreferencerepo

---
+++ server/src/integrationtest/java/org/jvalue/ods/db/mongodb/MongoDbProcessorChainReferenceRepositoryTest.java
@@ -0,0 +1,34 @@
+package org.jvalue.ods.db.mongodb;
+
+import org.jvalue.commons.db.DbConnectorFactory;
+import org.jvalue.commons.db.repositories.GenericRepository;
+import org.jvalue.commons.mongodb.test.AbstractRepositoryAdapterTest;
+import org.jvalue.ods.api.processors.ExecutionInterval;
+import org.jvalue.ods.api.processors.ProcessorReference;
+import org.jvalue.ods.api.processors.ProcessorReferenceChain;
+import org.jvalue.ods.db.mongodb.repositories.MongoDbProcessorChainReferenceRepository;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class MongoDbProcessorChainReferenceRepositoryTest extends AbstractRepositoryAdapterTest<ProcessorReferenceChain> {
+	@Override
+	protected GenericRepository<ProcessorReferenceChain> doCreateRepository(DbConnectorFactory connectorFactory) {
+		return new MongoDbProcessorChainReferenceRepository(connectorFactory, getClass().getSimpleName());
+	}
+
+
+	@Override
+	protected ProcessorReferenceChain doCreateValue(String id, String data) {
+		return new ProcessorReferenceChain(
+			id,
+			Arrays.asList(new ProcessorReference.Builder(data).build()),
+			new ExecutionInterval(0, TimeUnit.SECONDS));
+	}
+
+
+	@Override
+	protected void doDeleteDatabase(DbConnectorFactory dbConnectorFactory) {
+		dbConnectorFactory.doDeleteDatabase(getClass().getSimpleName());
+	}
+}
