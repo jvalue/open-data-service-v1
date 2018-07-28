aa6f4ae1aa7d17188196f6c20e38c745fd75d4e6
mathias <mathias.zinnen@gmail.com>
2018-07-28 21:27:00 +0200

    add from() and fromCollection() methods to DataSourceWrapper

--- server/src/main/java/org/jvalue/ods/rest/v2/jsonapi/wrapper/DataSourceWrapper.java
+++ server/src/main/java/org/jvalue/ods/rest/v2/jsonapi/wrapper/DataSourceWrapper.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.jvalue.ods.api.sources.DataSource;
 import org.jvalue.ods.api.sources.DataSourceMetaData;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 public class DataSourceWrapper extends DataSource implements JsonApiIdentifiable {
 
 
@@ -12,13 +15,42 @@ public class DataSourceWrapper extends DataSource implements JsonApiIdentifiable
 		super(id, domainIdKey, schema, metaData);
 	}
 
+
 	@Override
 	public String getId() {
 		return id;
 	}
 
+
 	@Override
 	public String getType() {
 		return DataSource.class.getSimpleName();
+	}
+
+
+	/**
+	 * Wrap a single DataSource
+	 * @param dataSource the source to wrap
+	 * @return a DataSourceWrapper which wraps {@param dataSource}
+	 */
+	public static DataSourceWrapper from(DataSource dataSource) {
+		return new DataSourceWrapper(
+			dataSource.getId(),
+			dataSource.getDomainIdKey(),
+			dataSource.getSchema(),
+			dataSource.getMetaData());
+	}
+
+
+	/**
+	 * Wrap a collection of DataSources
+	 * @param sourcesCollection the collection to wrap
+	 * @return a collection of DataSourcesWrappers wrapping the elements of {@param sourcesCollection}
+	 */
+	public static Collection<DataSourceWrapper> fromCollection(Collection<DataSource> sourcesCollection) {
+		return sourcesCollection
+				.stream()
+				.map(DataSourceWrapper::from)
+				.collect(Collectors.toList());
 	}
 }
