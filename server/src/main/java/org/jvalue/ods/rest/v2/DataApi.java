68fb72cad61d3bb3e39ec2c458c1e212ccef90dd
mathias <mathias.zinnen@gmail.com>
2018-07-29 21:31:17 +0200

    add DataApi v2 with implemented getObjects method()

---
+++ server/src/main/java/org/jvalue/ods/rest/v2/DataApi.java
@@ -0,0 +1,95 @@
+package org.jvalue.ods.rest.v2;
+
+
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
+import com.hubspot.jackson.jaxrs.PropertyFiltering;
+import org.jvalue.commons.auth.RestrictedTo;
+import org.jvalue.commons.auth.Role;
+import org.jvalue.commons.auth.User;
+import org.jvalue.commons.rest.RestUtils;
+import org.jvalue.ods.api.data.Data;
+import org.jvalue.ods.data.DataSourceManager;
+import org.jvalue.ods.db.DataRepository;
+import org.jvalue.ods.rest.v2.jsonapi.response.JsonApiResponse;
+import org.jvalue.ods.rest.v2.jsonapi.wrapper.DataWrapper;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import static org.jvalue.ods.rest.v2.AbstractApi.BASE_URL;
+
+@Path(BASE_URL + "/{sourceId}/data")
+public final class DataApi extends AbstractApi {
+
+	private final DataSourceManager sourceManager;
+
+	@Inject
+	public DataApi(DataSourceManager sourceManager) {
+		this.sourceManager = sourceManager;
+	}
+
+	@Context
+	private UriInfo uriInfo;
+
+
+	@GET
+	@PropertyFiltering(always = {"!result._id", "!result._rev"})
+	public Response getObjects(
+			@PathParam("sourceId") String sourceId,
+			@QueryParam("startId") String startId,
+			@QueryParam("count") int count,
+			@QueryParam("property") String propertyFilter) {
+
+		if (count < 1 || count > 100) throw RestUtils.createJsonFormattedException("count must be > 0 and <= 100", 400);
+
+		DataRepository repository = assertIsValidSource(sourceId);
+		Data data = repository.executePaginatedGet(startId, count);
+
+		return JsonApiResponse
+			.createGetResponse(uriInfo)
+			.data(DataWrapper.from(data))
+			.build();
+	}
+
+
+	@DELETE
+	public void deleteAllObjects(
+			@RestrictedTo(Role.ADMIN) User user,
+			@PathParam("sourceId") String sourceId) {
+
+		DataRepository repository = assertIsValidSource(sourceId);
+		repository.removeAll();
+	}
+
+
+	@GET
+	@Path("/{objectId}{pointer:(/.*)?}")
+	public JsonNode getObjectAttribute(
+			@PathParam("sourceId") String sourceId,
+			@PathParam("objectId") String domainId,
+			@PathParam("pointer") String pointer) {
+
+		JsonNode node = assertIsValidSource(sourceId).findByDomainId(domainId);
+		if (pointer.isEmpty() || pointer.equals("/")) return node;
+
+		try {
+			JsonPointer jsonPointer = JsonPointer.compile(pointer);
+			JsonNode result = node.at(jsonPointer);
+			if (result.isMissingNode()) throw RestUtils.createNotFoundException();
+			return result;
+		} catch (IllegalArgumentException iae) {
+			// thrown by JsonPointer.compile
+			throw RestUtils.createNotFoundException();
+		}
+	}
+
+
+	private DataRepository assertIsValidSource(String sourceId) {
+		return sourceManager.getDataRepository(sourceManager.findBySourceId(sourceId));
+	}
+
+}
