94d6102389a09c9a7b2fa2041d2c2cf17b8b443a
Julian Schneider <julian.js.schneider@fau.de>
2018-08-24 15:26:40 +0200

    added a test to detect running couchdb. Otherwise CouchDB views are not available.

--- server/src/main/java/org/jvalue/ods/rest/v1/DataViewApi.java
+++ server/src/main/java/org/jvalue/ods/rest/v1/DataViewApi.java
@@ -6,6 +6,7 @@ import org.jvalue.commons.auth.RestrictedTo;
 import org.jvalue.commons.auth.Role;
 import org.jvalue.commons.auth.User;
 import org.jvalue.commons.rest.RestUtils;
+import org.jvalue.commons.utils.HttpServiceCheck;
 import org.jvalue.ods.api.sources.DataSource;
 import org.jvalue.ods.api.views.couchdb.CouchDbDataView;
 import org.jvalue.ods.api.views.couchdb.CouchDbDataViewDescription;
@@ -20,6 +21,8 @@ import java.util.List;
 @Path(AbstractApi.BASE_URL + "/{sourceId}/views")
 @Produces(MediaType.APPLICATION_JSON)
 public final class DataViewApi extends AbstractApi {
+
+	private static final String COUCH_DB_NOT_AVAILABLE = "CouchDbView Api is not available. No CouchDB found. Use Transformation Api instead.";
 
 	private final DataSourceManager sourceManager;
 	private final DataViewManager viewManager;
@@ -36,6 +39,7 @@ public final class DataViewApi extends AbstractApi {
 
 	@GET
 	public List<CouchDbDataView> getAllViews(@PathParam("sourceId") String sourceId) {
+		assertCouchDbIsAvailable();
 		DataSource source = sourceManager.findBySourceId(sourceId);
 		return viewManager.getAll(source);
 	}
@@ -48,7 +52,7 @@ public final class DataViewApi extends AbstractApi {
 			@PathParam("viewId") String viewId,
 			@QueryParam("execute") boolean execute,
 			@QueryParam("argument") String argument) {
-
+		assertCouchDbIsAvailable();
 		DataSource source = sourceManager.findBySourceId(sourceId);
 		CouchDbDataView view = viewManager.get(source, viewId);
 
@@ -64,7 +68,7 @@ public final class DataViewApi extends AbstractApi {
 			@PathParam("sourceId") String sourceId,
 			@PathParam("viewId") String viewId,
 			@Valid CouchDbDataViewDescription viewDescription) {
-
+		assertCouchDbIsAvailable();
 		DataSource source = sourceManager.findBySourceId(sourceId);
 		if (viewManager.contains(source, viewId))
 			throw RestUtils.createJsonFormattedException("data view with id " + viewId + " already exists", 409);
@@ -81,10 +85,17 @@ public final class DataViewApi extends AbstractApi {
 			@RestrictedTo(Role.ADMIN) User user,
 			@PathParam("sourceId") String sourceId,
 			@PathParam("viewId") String viewId) {
-
+		assertCouchDbIsAvailable();
 		DataSource source = sourceManager.findBySourceId(sourceId);
 		CouchDbDataView view = viewManager.get(source, viewId);
 		viewManager.remove(source, sourceManager.getDataRepository(source), view);
+	}
+
+
+	private void assertCouchDbIsAvailable() {
+		if(!HttpServiceCheck.check(HttpServiceCheck.COUCHDB_URL, 1, 0)){
+			throw RestUtils.createJsonFormattedException(COUCH_DB_NOT_AVAILABLE, 503);
+		}
 	}
 
 }
