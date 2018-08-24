f78679bc822e9a48555c5a237527b7d89ccc7bc1
mathias <mathias.zinnen@gmail.com>
2018-08-24 15:37:48 +0200

    remove unneeded methods in JsonUtils

--- server/src/main/java/org/jvalue/ods/utils/JsonUtils.java
+++ server/src/main/java/org/jvalue/ods/utils/JsonUtils.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.jvalue.commons.utils.Assert;
-import org.jvalue.ods.rest.v2.jsonapi.response.JsonApiDocument;
 import org.jvalue.ods.rest.v2.jsonapi.wrapper.JsonApiIdentifiable;
 
 import java.io.IOException;
@@ -15,90 +13,40 @@ import java.util.Map;
 
 public class JsonUtils {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private JsonUtils() { }
-
-
-    public static JsonNode createRestrictedEntity (JsonApiIdentifiable entity, List<String> properties) {
-
-        ObjectNode originalNode = objectMapper.valueToTree(entity);
-        ObjectNode restrictedNode = objectMapper.createObjectNode();
-
-        properties.forEach(propertyName -> restrictedNode.set(propertyName, originalNode.get(propertyName)));
-
-        return restrictedNode;
-    }
-
-
-    public static JsonNode createRestrictedEntity (JsonApiIdentifiable entity, String property) {
-        List<String> properties = new LinkedList<>();
-        properties.add(property);
-        return createRestrictedEntity(entity, properties);
-    }
-
-
-    public static JsonNode createJsonNode(Object from) {
-    	Assert.assertNotNull(from);
-
-    	return objectMapper.valueToTree(from);
+	private JsonUtils() {
 	}
 
 
-	/**
-	 * Get a Map of all attributes of the entity which is contained in the given JsonApiDocument
-	 * @param doc the document which contains the entity
-	 * @return a map containing all attributes
-	 * @throws IOException
-	 */
-	public static Map<String, Object> getAttributesMap(JsonApiDocument doc) throws IOException {
-		Assert.assertNotNull(doc);
-		Assert.assertEquals(1, doc.getData().size(), "JsonApiDocument needs to contain exactly one entity.");
+	public static JsonNode createRestrictedEntity(JsonApiIdentifiable entity, List<String> properties) {
 
-		JsonNode entityNode = objectMapper.valueToTree(doc.getData().get(0).getEntity());
+		ObjectNode originalNode = objectMapper.valueToTree(entity);
+		ObjectNode restrictedNode = objectMapper.createObjectNode();
 
-		Map<String, Object> attributeMap = getMapFromJson(entityNode.get("attributes"));
+		properties.forEach(propertyName -> restrictedNode.set(propertyName, originalNode.get(propertyName)));
 
-		attributeMap.put("id", entityNode.get("id"));
-		attributeMap.put("type", entityNode.get("type"));
+		return restrictedNode;
+	}
 
-		return attributeMap;
+
+	public static JsonNode createRestrictedEntity(JsonApiIdentifiable entity, String property) {
+		List<String> properties = new LinkedList<>();
+		properties.add(property);
+		return createRestrictedEntity(entity, properties);
 	}
 
 
 	/**
 	 * Converts a given JsonNode to a map
+	 *
 	 * @param node the node to convert
 	 * @return a map containing each attribute of the node as entry
 	 */
-    public static Map<String, Object> getMapFromJson(JsonNode node) throws IOException {
-    	return objectMapper.readValue(
-    		node.toString(),
-			new TypeReference<Map<String, Object>>(){});
-	}
-
-
-	public static void assertIsValidJsonApiSingleDataDocument(JsonNode jsonApiDocument) {
-    	Assert.assertNotNull(jsonApiDocument);
-    	Assert.assertTrue( jsonApiDocument.has("data") );
-    	JsonNode dataNode = jsonApiDocument.get("data");
-		Assert.assertFalse( dataNode.isArray(), "document contains resource array but single resource object is expected" );
-		Assert.assertTrue(dataNode.has("id"));
-		Assert.assertTrue(dataNode.has("type"));
-    }
-
-
-	/**
-	 * get an attribute from a given JsonNode or an empty JsonNode if the attribute is not present
-	 * @param from the node to fetch from
-	 * @param attribute the attribute to get
-	 * @return the attribute as JsonNode or an empty JsonNode
-	 */
-    public static JsonNode getIfPresent(JsonNode from, String attribute) {
-    	if(from.has(attribute)) {
-    		return from.get(attribute);
-		} else {
-    		return objectMapper.createObjectNode();
-		}
+	public static Map<String, Object> getMapFromJson(JsonNode node) throws IOException {
+		return objectMapper.readValue(
+			node.toString(),
+			new TypeReference<Map<String, Object>>() {
+			});
 	}
 }
