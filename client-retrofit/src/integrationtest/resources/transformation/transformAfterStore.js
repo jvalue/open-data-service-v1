35e98282b0ae84de68ee1892c7a28aeaee77d414
Julian Schneider <julian.js.schneider@fau.de>
2018-10-11 16:25:26 +0200

    renamed emit() to output() to not get confused with couchdb view emit()

--- client-retrofit/src/integrationtest/resources/transformation/transformAfterStore.js
+++ client-retrofit/src/integrationtest/resources/transformation/transformAfterStore.js
@@ -7,7 +7,7 @@ function transform(doc) {
 		newdoc.duisburg.weather = doc.weatherDuisburg;
 		newdoc.extension = doc.extension;
 		newdoc.id = doc.someId;
-		emit(new Date().getTime(), newdoc);
-		emit(new Date().getTime(), { key: "value" });
+		output(newdoc);
+		output({ key: "value" });
 	}
 }
--- server/src/main/resources/js/NashornWrapper.js
+++ server/src/main/resources/js/NashornWrapper.js
@@ -11,17 +11,14 @@ function transformationWrapper(jsonStr, query) {
 	var json = JSON.parse(jsonStr);
 	var result = transform(json);
 	if(!query){
-		emit(new Date().getTime(),result);
+		output(result);
 	}
 	return resultSet;
 }
 
 //key must be a sortable value
-function emit(key, value){
-	keySet.push(key);
-	keySet.sort();
-	var index = keySet.indexOf(key);
-	resultSet.splice( index, 0, JSON.stringify(value));
+function output(value){
+	resultSet.push(JSON.stringify(value));
 }
 
 
