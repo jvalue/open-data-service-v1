1fd43aa4bfb6314c0767ac1efb390cfa579b2847
mathias <mathias.zinnen@gmail.com>
2018-11-08 13:17:12 +0100

    add equals() and hashCode() methods

--- server/src/main/java/org/jvalue/ods/rest/v2/jsonapi/response/JsonApiError.java
+++ server/src/main/java/org/jvalue/ods/rest/v2/jsonapi/response/JsonApiError.java
@@ -1,5 +1,7 @@
 package org.jvalue.ods.rest.v2.jsonapi.response;
 
+import java.util.Objects;
+
 public class JsonApiError {
 	private final String message;
 	private final int code;
@@ -15,5 +17,20 @@ public class JsonApiError {
 
 	public int getCode() {
 		return code;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		JsonApiError that = (JsonApiError) o;
+		return code == that.code &&
+			Objects.equals(message, that.message);
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(message, code);
 	}
 }
