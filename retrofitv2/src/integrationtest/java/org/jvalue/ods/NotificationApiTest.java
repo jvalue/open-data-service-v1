0c0ec9b7922425c7b5c9c616d659d626dc58b320
Andreas Bauer <dev@andreasbauer.org>
2018-11-28 15:10:53 +0100

    Adjust NotificationAPI and integration tests

--- retrofitv2/src/integrationtest/java/org/jvalue/ods/NotificationApiTest.java
+++ retrofitv2/src/integrationtest/java/org/jvalue/ods/NotificationApiTest.java
@@ -1,10 +1,13 @@
 package org.jvalue.ods;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import jsonapi.ResponseBody;
+import org.junit.Assert;
 import org.junit.Test;
 import org.jvalue.ods.api.notifications.*;
 import org.jvalue.ods.rest.v2.jsonapi.response.JsonApiRequest;
 import org.jvalue.ods.rest.v2.jsonapi.wrapper.ClientWrapper;
+import org.jvalue.ods.utils.JsonMapper;
 
 import java.io.IOException;
 
@@ -28,16 +31,28 @@ public class NotificationApiTest extends AbstractApiTest {
 	}
 
 
-	@Test
-	public void testUnregisterClient() {
-
+	public void doTestGetAllClients() {
 	}
+
 
 	private void doTestClient(Client client, ClientDescription clientDescription) throws IOException {
 		ClientWrapper wrapper = ClientWrapper.from(client);
 		JsonApiRequest request = JsonApiRequest.from(wrapper);
 
-		ResponseBody response = notificationApi.registerClient(sourceId, request);
+		notificationApi.registerClient(sourceId, request);
+		ResponseBody response = notificationApi.getClient(sourceId, client.getId());
+		Client result = requestToClient(response);
+
+		Assert.assertEquals(client, result);
+	}
+
+
+	private Client requestToClient(ResponseBody response) {
+		((ObjectNode) response.getData().get("attributes")).put("id", response.getId());
+		((ObjectNode) response.getData().get("attributes")).put("type", response.getType());
+		return JsonMapper.convertValue(
+			response.getData().get("attributes"),
+			Client.class);
 	}
 
 
