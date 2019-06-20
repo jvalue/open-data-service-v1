87a46238c15029f2cae1cd68c6c6826b38f46c01
daniel <daniel.vahle@fau.de>
2019-06-20 21:18:24 +0200

    Add SunshineDuration.

---
+++ server/src/main/java/org/jvalue/ods/processor/adapter/domain/weather/models/extended/SunshineDuration.java
@@ -0,0 +1,45 @@
+package org.jvalue.ods.processor.adapter.domain.weather.models.extended;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+public class SunshineDuration {
+	private final double value;
+	private final DurationType type;
+
+	@JsonCreator
+	public SunshineDuration(
+		@JsonProperty("value") double value,
+		@JsonProperty("type") DurationType type) {
+		this.value = value;
+		this.type = type;
+	}
+
+
+	public double getValue() {
+		return value;
+	}
+
+
+	public DurationType getType() {
+		return type;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		SunshineDuration sunshineDuration = (SunshineDuration) o;
+		return Double.compare(sunshineDuration.value, value) == 0 &&
+			type == sunshineDuration.type;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value, type);
+	}
+}
