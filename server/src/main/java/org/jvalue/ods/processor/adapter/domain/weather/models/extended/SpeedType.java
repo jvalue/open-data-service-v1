1da5e8950a8f1cd3061466e6394c32261d1635c8
daniel <daniel.vahle@fau.de>
2019-06-20 21:43:05 +0200

    Fix SpeedType.

--- server/src/main/java/org/jvalue/ods/processor/adapter/domain/weather/models/extended/SpeedType.java
+++ server/src/main/java/org/jvalue/ods/processor/adapter/domain/weather/models/extended/SpeedType.java
@@ -1,8 +1,8 @@
 package org.jvalue.ods.processor.adapter.domain.weather.models.extended;
 
 public enum SpeedType {
-	METER_PER_SECOND("km / h"),
-	KILOMETER_PER_HOUR("m / s");
+	METER_PER_SECOND("m / s"),
+	KILOMETER_PER_HOUR("km / h");
 
 	private final String type;
 
